1953d2248ff1ff0eb6486431b2262c1e97bf6846
mattquesada <quesadamatthew@yahoo.com>
2019-02-20 01:22:25 -0800

    logging

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const port = process.env.PORT || 5000;
 const db = require('./database/User');
 
 app.use(bodyParser.json());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
   response.json({info: 'Node.js, Express, Postgres API'});
