65931cc711ff8767ae7f798a444dfebb7c48734d
Matthew Quesada <quesadamatthew@yahoo.com>
2019-02-24 23:50:49 -0800

    Added addImage() and updateCaption() queries to postgres

---
+++ database/Image.js
@@ -0,0 +1,41 @@
+const utils = require('./Utils');
+
+const addImage = (request, response) => {
+  const { userID, imageURL } = request.body;
+  const currentTime = new Date().toLocaleString();
+
+  const query = 
+  ` 
+    INSERT INTO images (ImageURL, UserID, DatePosted)
+    VALUES ('${imageURL}', '${userID}', '${currentTime}')
+  `;
+
+  client.connect();
+  client.query(query, (error, results) => {
+    if (error) throw error;
+    response.status(201).send(`Image added successfully`);
+    client.end();
+  });
+}
+
+const updateCaption = (request, response) => {
+  const { imageID, caption } = request.body;
+  const query = 
+  `
+    UPDATE images 
+    SET Caption = '${caption}'
+    WHERE ImageID = '${imageID}'
+  `;
+
+  client.connect();
+  client.query(query, (error, results) => {
+    if (error) throw error;
+    response.status(201).send(`Caption updated successfully`);
+    client.end();
+  });
+}
+
+module.exports = {
+  addImage,
+  updateCaption
+}
