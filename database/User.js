a20cd60193abab7abac74ac468896597b9620b8f
mattquesada <quesadamatthew@yahoo.com>
2019-02-25 02:14:45 -0800

    process.env test

--- database/User.js
+++ database/User.js
@@ -101,7 +101,7 @@ const removeFollow = (request, response) => {
 
 const getFollowers = (request, response) => {
   const client = utils.initClient();
-  const username = request.body.username;
+  const username = request.query['username'];
   const query = ` SELECT isFollowedUsername
                   FROM follows
                   WHERE followUsername = '${username}'`;
